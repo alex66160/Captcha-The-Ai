f44b32399d177f653591bdb51af391d5b6b2c561
Alex Liu <alexyliu2@gmail.com>
2026-08-26 01:08:50 -0400

    Finished answer components on frontend #57

--- frontend/src/components/phases/Answer.tsx
+++ frontend/src/components/phases/Answer.tsx
@@ -1,5 +1,58 @@
+import { useContext, useState } from "react";
+import { lobbyContext } from "../LobbyContext";
+import { publish } from "../StompActions";
+import { useParams } from "react-router-dom";
+
+type SubmitAnswerRequest = { answer: string };
+
 function Answer() {
-    return <p>answer</p>;
+    const lobbyState = useContext(lobbyContext);
+    const lobbyId = useParams().lobbyId;
+    const [answer, setAnswer] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    if (lobbyState === null) {
+        throw new Error("Lobby state was null in answer component");
+    }
+    const selfPlayer = lobbyState.players.filter((player) => player.isSelf)[0];
+
+    if (selfPlayer.isQuestionWriter && selfPlayer.playerStatus === "ALIVE") {
+        return <p>Please wait while players finish answering your question.</p>;
+    }
+
+    if (!selfPlayer.isQuestionWriter && selfPlayer.playerStatus === "ALIVE") {
+        return (
+            <div>
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        publish<SubmitAnswerRequest>(
+                            `/app/lobbies/${lobbyId}/answers`,
+                            { answer: answer },
+                        );
+                        setIsSubmitted(true);
+                    }}
+                >
+                    <input
+                        type="text"
+                        maxLength={100}
+                        value={answer}
+                        onChange={(event) => setAnswer(event.target.value)}
+                    ></input>
+
+                    <button
+                        type="submit"
+                        disabled={answer.trim() === "" || isSubmitted}
+                    >
+                        Im finished!
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    if (selfPlayer.playerStatus === "SPECTATOR") {
+        return <p>Players are currently answering the question...</p>;
+    }
 }
 
 export default Answer;
--- frontend/src/components/phases/AnswerAnnouncement.tsx
+++ frontend/src/components/phases/AnswerAnnouncement.tsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import { lobbyContext } from "../LobbyContext";
+
 function AnswerAnnouncement() {
-    return <p>Answer announcement</p>;
+    const lobbyState = useContext(lobbyContext);
+    if (lobbyState === null) {
+        throw new Error(
+            "Lobby State was null in answer announcement component",
+        );
+    }
+    const selfPlayer = lobbyState.players.filter((player) => player.isSelf)[0];
+    if (selfPlayer.isQuestionWriter && selfPlayer.playerStatus === "ALIVE") {
+        return <p>Players will now begin answering your question!</p>;
+    }
+
+    if (!selfPlayer.isQuestionWriter && selfPlayer.playerStatus === "ALIVE") {
+        return <p>Get ready to answer the question!</p>;
+    }
+
+    if (selfPlayer.playerStatus === "SPECTATOR") {
+        return <p>Players will now begin answering the question!</p>;
+    }
 }
 
 export default AnswerAnnouncement;
